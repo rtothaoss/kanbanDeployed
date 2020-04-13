5ac39fb4729ffd8897b2788d5fda5821bbcbdce8
Ross carmack <rosscarmack@rosss-mbp.home>
2020-04-13 13:59:16 -0500

    added react files

---
+++ src/main/resources/static/precache-manifest.d2fe7ad725d8e44c248c706090bb036e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86fcd0d920b559f36195257938a126c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f16fa3bad7ce177c4049",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b8f94c316a8470f6053c",
+    "url": "/static/css/main.d242b985.chunk.css"
+  },
+  {
+    "revision": "f16fa3bad7ce177c4049",
+    "url": "/static/js/2.5b2b1aff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5b2b1aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8f94c316a8470f6053c",
+    "url": "/static/js/main.779ccd98.chunk.js"
+  },
+  {
+    "revision": "e6833b4702fc462ee713",
+    "url": "/static/js/runtime-main.c8108b31.js"
+  }
+]);
